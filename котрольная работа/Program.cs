757ac59ce9ab53b4d979e6de73b006c87c0b49d9
muhammadkodyr <okk972016@gmail.com>
2022-12-28 00:08:09 +0500

    not finished one

---
+++ котрольная работа/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] mass = {"jojo", ":-)", "ko", "php"};
+string[] result = {};
+Getresult(mass);
+
+string[] Getresult (string[] mass)
+{
+   for (int i = 0; i <= mass.Length; i++) 
+   {
+       if(mass[i].Length >= 3)
+       {
+        result = mass[i];
+       }
+   }
+    return (result);
+}
