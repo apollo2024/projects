ee66bc48cf1660669434d9ae54896b54b4698352
muhammadkodyr <okk972016@gmail.com>
2022-12-28 11:38:27 +0500

    исправили ошибки в projects

--- котрольная работа/Program.cs
+++ котрольная работа/Program.cs
@@ -6,7 +6,7 @@ void Getresult (string[] mass)
 {
    for (int i = 0; i <= mass.Length; i++) 
    {
-       if(mass[i].Length >= 3)
+       if(mass[i].Length <= 3)
        {
         System.Console.WriteLine((mass[i]) );
        }
