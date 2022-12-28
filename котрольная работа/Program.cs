a05a72391da7623adaa41f0b1946f9333becab67
muhammadkodyr <okk972016@gmail.com>
2022-12-28 09:35:50 +0500

    the final one

--- котрольная работа/Program.cs
+++ котрольная работа/Program.cs
@@ -4,5 +4,11 @@ Getresult(mass);
 
 void Getresult (string[] mass)
 {
-  
+   for (int i = 0; i <= mass.Length; i++) 
+   {
+       if(mass[i].Length >= 3)
+       {
+        System.Console.WriteLine((mass[i]) );
+       }
+   }
 }
