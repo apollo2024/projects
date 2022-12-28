b11d4daacc28592be66fe23654545919f027a25b
muhammadkodyr <okk972016@gmail.com>
2022-12-28 09:33:47 +0500

    try to make with type void

--- котрольная работа/Program.cs
+++ котрольная работа/Program.cs
@@ -1,15 +1,8 @@
 ﻿string[] mass = {"jojo", ":-)", "ko", "php"};
-string[] result = {};
+
 Getresult(mass);
 
-string[] Getresult (string[] mass)
+void Getresult (string[] mass)
 {
-   for (int i = 0; i <= mass.Length; i++) 
-   {
-       if(mass[i].Length >= 3)
-       {
-        result = mass[i];
-       }
-   }
-    return (result);
+  
 }
